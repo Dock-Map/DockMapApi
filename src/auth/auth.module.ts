2908461d6138caf3741c1c5d5ab15e38866f6650
Philip Veremeichik <p.veremeichik@richbrains.net>
2025-08-06 12:14:15 +0300

    init auth vk

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,7 +8,6 @@ import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthService } from './services/auth.service';
 import { TokenService } from './services/token.service';
 import { SmsService } from './services/sms.service';
-import { VkService } from './services/vk.service';
 import { expiresAccessIn } from 'src/shared/constants';
 import { JwtRefreshTokenStrategy } from './strategies/jwt-refresh.strategy';
 import { UserModule } from '../user/user.module';
@@ -37,7 +36,6 @@ import { VerificationCode } from './entities/verification-code.entity';
     AuthService,
     TokenService,
     SmsService,
-    VkService,
     ConfigService,
     JwtStrategy,
     JwtRefreshTokenStrategy,
