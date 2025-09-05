26a4d12c0669bc5860b5cae8fb609ac040a4e919
Philip Veremeichik <p.veremeichik@richbrains.net>
2025-09-05 21:04:21 +0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HealthCheckModule } from 'src/healthCheck/healthCheck.module';
 import { UserModule } from 'src/user/user.module';
 import { AuthModule } from 'src/auth/auth.module';
-import { SharedModule } from 'src/shared/shared.module';
+// import { SharedModule } from 'src/shared/shared.module';
 import { User } from 'src/user/entities/user.entity';
 import { VerificationCode } from 'src/auth/entities/verification-code.entity';
 
@@ -20,34 +20,25 @@ import { VerificationCode } from 'src/auth/entities/verification-code.entity';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
-        // Дефолтные значения для Timeweb Cloud Apps
-        const isProduction = process.env.NODE_ENV === 'production';
-
         return {
           type: 'postgres',
-          host:
-            configService.get<string>('DATABASE_HOST') ||
-            (isProduction ? '192.168.0.5' : 'localhost'),
-          port: configService.get<number>('DATABASE_PORT') || 5432,
-          username:
-            configService.get<string>('DATABASE_USER') ||
-            (isProduction ? 'dock_prod' : 'dock'),
-          password:
-            configService.get<string>('DATABASE_PASSWORD') ||
-            (isProduction ? 'dock_prod' : 'dock'),
-          database:
-            configService.get<string>('DATABASE_NAME') ||
-            (isProduction ? 'dock_prod' : 'dock'),
+          host: configService.get<string>('DATABASE_HOST'),
+          port: configService.get<number>('DATABASE_PORT'),
+          username: configService.get<string>('DATABASE_USER'),
+          password: configService.get<string>('DATABASE_PASSWORD'),
+          database: configService.get<string>('DATABASE_NAME'),
           synchronize: true,
-          entities: [User, VerificationCode],
-          ssl: isProduction ? { rejectUnauthorized: false } : false,
+          entities: [
+            User,
+            VerificationCode,
+          ],
         };
       },
     }),
     HealthCheckModule,
     UserModule,
     AuthModule,
-    SharedModule,
+    // SharedModule,
   ],
   controllers: [AppController],
   providers: [AppService],
